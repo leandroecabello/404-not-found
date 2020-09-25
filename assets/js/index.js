908c0afd1dc699853b20ea35eec969643e749ec4
leandro <leandro.ecabello@gmail.com>
2020-09-24 21:30:50 -0300

    modified index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 let prevUrl = document.referrer;
 let buttonReturn = document.querySelector('#button')
 
+console.log(prevUrl)
+console.log(prevUrl.indexOf(window.location.host) === -1)
 buttonReturn.addEventListener('click', (e) => {
     e.preventDefault()
-    if(prevUrl.indexOf(window.location.host) !== -1) {
+    if(prevUrl.indexOf(window.location.host) === -1) {
         // Ir a la página anterior   
         return location.href = prevUrl
     }
